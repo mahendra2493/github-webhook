dc5d17d8ed695480f779222d40d2eb153fedce4f
mahendra2493 <58428554+mahendra2493@users.noreply.github.com>
2020-01-31 10:12:20 +0530

    Update mahendra.sh

--- mahendra.sh
+++ mahendra.sh
@@ -1,1 +1,37 @@
-echo "hi this is working  mahendra<123456789>"
+#!/bin/sh
+
+# Output colors
+red='\033[0;31m';
+green='\033[0;32m';
+yellow='\033[0;33m';
+default='\033[0;m';
+
+# personal touch :-)
+echo "${red}"
+echo "                                         "
+echo "                   |ZZzzz                "
+echo "                   |                     "
+echo "                   |                     "
+echo "      |ZZzzz      /^\            |ZZzzz  "
+echo "      |          |~~~|           |       "
+echo "      |        |-     -|        / \      "
+echo "     /^\       |[]+    |       |^^^|     "
+echo "  |^^^^^^^|    |    +[]|       |   |     "
+echo "  |    +[]|/\/\/\/\^/\/\/\/\/|^^^^^^^|   "
+echo "  |+[]+   |~~~~~~~~~~~~~~~~~~|    +[]|   "
+echo "  |       |  []   /^\   []   |+[]+   |   "
+echo "  |   +[]+|  []  || ||  []   |   +[]+|   "
+echo "  |[]+    |      || ||       |[]+    |   "
+echo "  |_______|------------------|_______|   "
+echo "                                         "
+echo "                                         "
+echo "  ${green}                               "
+echo "  You have to commit your code   " 
+echo "  To Gitlab as well !!!!!        "
+echo "                                 "
+echo "  ${red}                         "
+echo "  P.S: Your code is bad.         "
+echo "       Do not ever commit again  "
+echo "                                 "
+echo "                                 "
+echo "${default}"
